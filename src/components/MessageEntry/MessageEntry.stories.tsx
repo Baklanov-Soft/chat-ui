ab15093bfe00f07fb5ffc5819151f057bef5e3dc
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2021-06-23 23:44:38 +0500

    fix unnecessary arg

--- src/components/MessageEntry/MessageEntry.stories.tsx
+++ src/components/MessageEntry/MessageEntry.stories.tsx
@@ -7,7 +7,6 @@ export const Primary = Template.bind({});
 Primary.args = {
   avatar:
     'https://3.bp.blogspot.com/-yTF-5A3kAH8/WlCLjrizoeI/AAAAAAAC68Q/BxEnavDjqWYLI22b2rTOdA6wVUIwEIkjACLcBGAs/s1600/dfgh.jpg',
-  id: 1,
   user: 'Rarity',
   text:
     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, voluptatum illo at accusantium maiores ratione?',
