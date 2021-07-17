6c8fd9c1f792db9303bcb38de67e012353d6dbcb
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2021-07-17 18:13:10 +0500

    resolver refactoring

--- src/components/CreateAccountForm/resolver.tsx
+++ src/components/CreateAccountForm/resolver.tsx
@@ -1,55 +1,39 @@
 import type { Resolver } from 'react-hook-form';
 import type { CreateAccountInput } from './CreateAccountForm';
 
+type CreateAccountInputErrors = {
+  [K in keyof CreateAccountInput]: { message: string };
+};
+
 export const resolver: Resolver<CreateAccountInput> = ({
   username,
   email,
   password1,
   password2,
 }) => {
+  const errors: CreateAccountInputErrors = {};
   if (!username) {
-    return {
-      values: {},
-      errors: {
-        username: { message: 'username is required' },
-      },
-    };
+    errors.username = { message: 'username is required' };
   }
-  if (email || (email && !isValidEmail(email))) {
-    return {
-      values: {},
-      errors: {
-        email: { message: 'invalid email' },
-      },
-    };
+  if (email && !isValidEmail(email)) {
+    errors.email = { message: 'invalid email' };
   }
   if (!password1) {
-    return {
-      values: {},
-      errors: {
-        password1: { message: 'password empty' },
-      },
-    };
+    errors.password1 = { message: 'password empty' };
   }
   if (!password2) {
-    return {
-      values: {},
-      errors: {
-        password2: { message: 'confirm is required' },
-      },
-    };
+    errors.password2 = { message: 'confirm is required' };
   }
   if (password1 && password2 && password1 !== password2) {
-    return {
-      values: {},
-      errors: {
-        password2: { message: 'password mismatch' },
-      },
-    };
+    errors.password2 = { message: 'password mismatch' };
+  }
+  if (errors.username || errors.email || errors.password1 || errors.password2) {
+    return { values: {}, errors };
   }
   return { values: { username, email, password1, password2 }, errors: {} };
 };
 
 function isValidEmail(email: string) {
-  return /^[\w-.]+@([\w-]+\.)+\w+$/.test(email);
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
